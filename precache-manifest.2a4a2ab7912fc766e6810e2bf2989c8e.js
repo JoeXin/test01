724e201d630b8e777e6e248cbe1591db4a803fc6
JoeXin <1203970535@qq.com>
2021-12-03 02:17:20 +0000

    Deploying to gh-pages from @ JoeXin/test01@9ef96a9c040438caf87dba7d38fb2b720f418d62 🚀

---
+++ precache-manifest.2a4a2ab7912fc766e6810e2bf2989c8e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18ba5ace0a25be40ecb9120f39d5e981",
+    "url": "/test01/index.html"
+  },
+  {
+    "revision": "200b318c3bf392631b09",
+    "url": "/test01/static/css/2.e7ac3521.chunk.css"
+  },
+  {
+    "revision": "ea5acea89c984362c321",
+    "url": "/test01/static/css/main.becc7b78.chunk.css"
+  },
+  {
+    "revision": "200b318c3bf392631b09",
+    "url": "/test01/static/js/2.d216c90a.chunk.js"
+  },
+  {
+    "revision": "4cf5c6c6f464cc15458a86cbdc669a15",
+    "url": "/test01/static/js/2.d216c90a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea5acea89c984362c321",
+    "url": "/test01/static/js/main.996a7325.chunk.js"
+  },
+  {
+    "revision": "c62b58b77f997a6ecec1",
+    "url": "/test01/static/js/runtime-main.54b7e2af.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test01/static/media/logo.5d5d9eef.svg"
+  }
+]);
