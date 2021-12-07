00b957494a8fe5ccca21d8e23755f212ce9884aa
JoeXin <1203970535@qq.com>
2021-12-07 06:36:41 +0000

    Deploying to gh-pages from @ JoeXin/test01@08098a3b1f810eb992a25c21872fdaedb2587582 🚀

---
+++ precache-manifest.ebf46ebfb97762ab49be90dcb92b5ad2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84aaa30c97a7a78d79fc509d5a2b8796",
+    "url": "/test01/index.html"
+  },
+  {
+    "revision": "f3d849a77350322742d7",
+    "url": "/test01/static/css/2.e7ac3521.chunk.css"
+  },
+  {
+    "revision": "ea5acea89c984362c321",
+    "url": "/test01/static/css/main.becc7b78.chunk.css"
+  },
+  {
+    "revision": "f3d849a77350322742d7",
+    "url": "/test01/static/js/2.c4b2c686.chunk.js"
+  },
+  {
+    "revision": "4cf5c6c6f464cc15458a86cbdc669a15",
+    "url": "/test01/static/js/2.c4b2c686.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea5acea89c984362c321",
+    "url": "/test01/static/js/main.996a7325.chunk.js"
+  },
+  {
+    "revision": "c62b58b77f997a6ecec1",
+    "url": "/test01/static/js/runtime-main.54b7e2af.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test01/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0e9bef415f4c83bab51829f357591092.js"
+  "/test01/precache-manifest.ebf46ebfb97762ab49be90dcb92b5ad2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test01/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
